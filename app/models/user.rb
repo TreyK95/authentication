4864be851a03fe36fd5e18185994226e1a300114
zyzzyxis <13ezimm@gmail.com>
2021-03-25 15:54:43 -0600

    added buddies to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  serialize :added_buddies, Array
+
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  def self.get_added_buddies(ids)
+    ids = ids.empty? ? [0] : ids
+    Buddy.where("id NOT IN (?)", ids).order("RANDOM()")
+  end
+
+  def self.added(ids)
+    ids = ids.empty? ? [0] : ids
+    Buddy.where("id IN (?)", ids)
+  end
+
 end

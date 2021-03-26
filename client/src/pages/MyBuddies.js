e8c75c7fada9c8f7bd0378a0b3755dc2b182b674
zyzzyxis <13ezimm@gmail.com>
2021-03-26 12:03:23 -0600

    moved my buddies to people

--- client/src/pages/MyBuddies.js
+++ client/src/pages/MyBuddies.js
@@ -10,63 +10,12 @@ import { AuthContext } from '../providers/AuthProvider'
 
 
 const MyBuddies = () => {
-  const [buddies, setBuddies] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const { user } = useContext(AuthContext)
   
-  useEffect(()=>{
-    getData()
-  },[])
-
-  const getData = async() => {
-    try{
-      let res = await axios.get('/api/buddies')
-      console.log('res buddies')
-      console.log('res.data', res.data)
-      setBuddies(res.data)
-     
-      setLoading(false)
-    } catch(err) {
-      alert('err')
-      setLoading(false)
-    }
-  }
-  // const buddy =  buddy[Math.floor(Math.random() * buddies.length)]
-  
-  const removeBuddy = (id) => {
-    const newBuddies = buddies.filter( buddy => buddy.id !== id)
-    setBuddies(newBuddies)
-  }
-
-  // const addBuddy = (id) => {}
-  if(loading) return <p>Loading potential buddies, please wait</p>
-  // if(buddies !== null && buddies.length < 1 ) return <p>No more potential buddies</p>
-  
-  const renderBuddies = () => {
-    if(buddies !== null && buddies.length > 1){
-      console.log('render buddy test1')
-      return buddies.map(buddy =>{
-        console.log('render buddy test 2')
-          // console.log({buddy})
-        return(
-          <Card>
-            <Card.Header>{buddy.name}</Card.Header>
-            <Card.Content>
-              <Image src={buddy.avatar} />
-            </Card.Content>
-            <Card.Description>
-              {buddy.email}
-            </Card.Description>
-          </Card>
-        )
-      })
-    }
-  }
   
   return (
     <div>
       <h1>MyBuddies</h1>
-      {renderBuddies()}
+     
 
       
     </div>

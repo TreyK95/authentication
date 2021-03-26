db16b85cb16b9414b2380395e4b8521c21de5b83
zyzzyxis <13ezimm@gmail.com>
2021-03-26 11:54:54 -0600

    buddies rendering

--- client/src/pages/MyBuddies.js
+++ client/src/pages/MyBuddies.js
@@ -2,14 +2,71 @@
 // can remove friends from this page
 
 
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useContext, useEffect} from 'react'
+import { Card, Header, Image } from 'semantic-ui-react'
+
+import { AuthContext } from '../providers/AuthProvider'
+
 
 const MyBuddies = () => {
+  const [buddies, setBuddies] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const { user } = useContext(AuthContext)
+  
+  useEffect(()=>{
+    getData()
+  },[])
+
+  const getData = async() => {
+    try{
+      let res = await axios.get('/api/buddies')
+      console.log('res buddies')
+      console.log('res.data', res.data)
+      setBuddies(res.data)
+     
+      setLoading(false)
+    } catch(err) {
+      alert('err')
+      setLoading(false)
+    }
+  }
+  // const buddy =  buddy[Math.floor(Math.random() * buddies.length)]
+  
+  const removeBuddy = (id) => {
+    const newBuddies = buddies.filter( buddy => buddy.id !== id)
+    setBuddies(newBuddies)
+  }
+
+  // const addBuddy = (id) => {}
+  if(loading) return <p>Loading potential buddies, please wait</p>
+  // if(buddies !== null && buddies.length < 1 ) return <p>No more potential buddies</p>
+  
+  const renderBuddies = () => {
+    if(buddies !== null && buddies.length > 1){
+      console.log('render buddy test1')
+      return buddies.map(buddy =>{
+        console.log('render buddy test 2')
+          // console.log({buddy})
+        return(
+          <Card>
+            <Card.Header>{buddy.name}</Card.Header>
+            <Card.Content>
+              <Image src={buddy.avatar} />
+            </Card.Content>
+            <Card.Description>
+              {buddy.email}
+            </Card.Description>
+          </Card>
+        )
+      })
+    }
+  }
+  
   return (
     <div>
       <h1>MyBuddies</h1>
-      <p>will render buddies that have been friended already</p>
-      
+      {renderBuddies()}
 
       
     </div>
